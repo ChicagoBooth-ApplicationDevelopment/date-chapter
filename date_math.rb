4807c9cfdbe740b4b683b5ed30b2c753e7fa28e9
syinGit <syin1@chicagobooth.edu>
2021-07-22 01:45:57 +0000

    date-chapter_final

--- date_math.rb
+++ date_math.rb
@@ -5,3 +5,11 @@
 # Output:
 #   "Ruby is 108937 days old!"
 #
+
+require "date"
+
+age_indays = Date.today - Date.parse("December 21, 1995")
+
+age_indays = age_indays.to_i
+
+p "Ruby is #{age_indays} days old!"
